1b8229c7a95b4ac25e54de2d5f21ad48063049fc
Maria <m.ringes@me.com>
2021-11-22 23:09:59 -0500

    Comments

--- ledger.go
+++ ledger.go
@@ -12,6 +12,13 @@ type Logger struct {
 	mailbox       *chan Message
 }
 
+/*
+	@input nonce int // An integer value that is claimed to have solved the puzzle
+		   provider string // A string that contains the identify of the miner who has claimed to solve the puzzle
+		   prevBlock // A Block instance that will serve as the previous block to this newly created block
+	@output Block // A block with a blockHeader and a transaction relating to the miner who has claimed to solve the puzzle
+	NewBlock is a function that given a nonce and the identity of a miner can create a new block instance to be proposed to the Logger for validation
+*/
 func NewBlock(nonce int, provider string, prevBlock Block) Block {
 	var prevBlockHashPointer HashPointer
 	prevBlockHashPointer.hash = sha256.Sum256(HeaderToByteSlice(prevBlock.blockHeader)) 
@@ -27,6 +34,12 @@ func NewBlock(nonce int, provider string, prevBlock Block) Block {
 	return Block{newHeader, transaction}
 }
 
+/*
+	@input nonce int // An integer value that is claimed to have solved the puzzle
+		   provider string // A string that contains the identify of the miner who has claimed to solve the puzzle
+		   miners []Miner // A slice that contains the miners of the blockchain
+	UpdateBlock is a function that calculates in how much time the puzzle was solved. Additionally, this function updates the logger's current block hash to be worked on.
+*/
 func (l *Logger) UpdateBlock(nonce int, provider string, miners []Miner) {
 	l.block = NewBlock(nonce, provider, l.block)
 	puzzlesolvedin := time.Since(newpuzzle).Seconds()
@@ -42,11 +55,21 @@ func (l *Logger) UpdateBlock(nonce int, provider string, miners []Miner) {
 	}
 }
 
+/*
+	@input nonce int // An integer value that is claimed to have solved the puzzle
+	@output bool // A boolean value that will store the information as to whether the proposed nonce has solved the puzzle
+	CheckNonce is a function that will compare the hashOutput gotten from hashing the concatenation of the currentBlockHash with the proposed nonce
+*/
 func (l Logger) CheckNonce(nonce int) bool {
 	hashOutput := sha256.Sum256(append(l.currBlockHash[:], IntToByteSlice(nonce)...))
 	return Compare(hashOutput[:], l.block.blockHeader.bits[:]) < 0
 }
 
+/*
+	@input miners []Miner //A slice that contains the miners of the blockchain
+	ListenForUpdate is a function that checks to see if the any messages have been sent to the Logger for verification
+	This function also checks whether or not the blockchain should terminate in the interest of time
+*/
 func (l Logger) ListenForUpdate(miners []Miner) {
 	fmt.Println("Logger has begun listening for new updates.")
 	for true {
